444a47b2072f20b2fbf21d64a8432fb10b2cf680
Christopher Li <cli135@jhu.edu>
2023-05-28 11:49:59 -0400

    Add sphere

--- main.cpp
+++ main.cpp
@@ -6,10 +6,27 @@
 
 using namespace std;
 
+bool hit_sphere(const point3 & center, double radius, const ray & r);
+color ray_color(const ray & r);
+
 color ray_color(const ray & r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0);
+  }
   vec3 unit_direction = unit_vector(r.direction());
   double t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+bool hit_sphere(const point3 & center, double radius, const ray & r) {
+  // helper shortcut
+  vec3 oc = r.origin() - center; // (A - C)
+  // quadratic coeffiecients
+  double a = dot(r.direction(), r.direction()); // (b . b)
+  double b = 2 * dot(r.direction(), oc); // (2 * b . (A - C))
+  double c = dot(oc, oc) - radius * radius; // (A - C) . (A - C) - r^2
+  double discriminant = b * b - 4 * a * c; // quadratic
+  return discriminant > 0; // tangent lines we ignore
 }
 
 int main() {
